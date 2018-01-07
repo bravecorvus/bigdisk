f3652175605c07e364d63cb724c2ee996c18eeb2
gilgameshkytrooper <leeas@stolaf.edu>
2018-01-07 01:06:20 -0600

    added Exists function to utils

--- bigdisk.go
+++ bigdisk.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -12,9 +11,11 @@ func main() {
 	fs := http.FileServer(http.Dir(utils.Pwd() + "/public"))
 	http.Handle("/", fs)
 	log.Println("Listening...")
-	tlserr := http.ListenAndServeTLS(":3000", "server.crt", "server.key", nil)
-	if tlserr != nil {
-		fmt.Println("If you want the program to utilize TLS (i.e. host an encrypted HTTPS front end, please do the following in command line in the same directory as prometheus.go to first create a private self-signed rsa key, then a public key (x509) key based on the private key:\n\topenssl genrsa -out server.key 2048\n\topenssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650\nThen make sure you finish filling in the details asked in command line.\n\nFor now, unencrypted http will be used.")
-		log.Fatal(http.ListenAndServe(":3000", nil))
-	}
+	// tlserr := http.ListenAndServeTLS(":8080", utils.Pwd()+"/server.crt", utils.Pwd()+"/server.key", nil)
+	// if tlserr != nil {
+	// os.Exit(1)
+	// fmt.Println("If you want the program to utilize TLS (i.e. host an encrypted HTTPS front end, please do the following in command line in the same directory as prometheus.go to first create a private self-signed rsa key, then a public key (x509) key based on the private key:\n\topenssl genrsa -out server.key 2048\n\topenssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650\nThen make sure you finish filling in the details asked in command line.\n\nFor now, unencrypted http will be used.")
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	// }
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- utils/utils.go
+++ utils/utils.go
@@ -17,3 +17,14 @@ func Pwd() string {
 	}
 	return dir
 }
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
